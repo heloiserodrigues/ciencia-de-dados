a0424d0450ef3791dc01e46a2959a6fa3b04a84d
heloiserodrigues <111513024+heloiserodrigues@users.noreply.github.com>
2024-10-10 07:56:43 -0300

    ok

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -5,9 +5,12 @@ async function visualizarInformacoesGlobais() {
         const res = await fetch(url)
 
         const dados = await res.json()
-
-        console.log(dados);
-
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem ${dado.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas}
+estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas.conectadas.`
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo)
     }
 
     visualizarInformacoesGlobais()

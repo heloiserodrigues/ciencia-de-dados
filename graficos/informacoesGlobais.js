e6d114e74796282257227522705160ff86d56493
heloiserodrigues <111513024+heloiserodrigues@users.noreply.github.com>
2024-10-24 08:46:07 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,13 +4,12 @@ async function visualizarInformacoesGlobais() {
 
         const res = await fetch(url)
 
-        const dados = await res.json()
+        const dados = res.json()
+        const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9 
+        const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
         const paragrafo = document.createElement('p')
         paragrafo.classList.add('graficos-container__texto')
-        paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas}
-estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas.conectadas.`
+        paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente <span> ${pessoasConectadas} bilhões </span> estão conectadas em alguma rede social e passam em média <span> ${horas} </span> horas e  <span> ${minutos} </span> minutos conectadas. <br> Isso significa que aproximadamente ${porcentagemConectada} % de pessoas estão conectadas em alguma rede social.`
         const container = document.getElementById('graficos-container')
         container.appendChild(paragrafo)
 }
-
-visualizarInformacoesGlobais()
